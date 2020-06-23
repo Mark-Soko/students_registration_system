db5ebfee0cbab8ec7cf72d63eef3439372f0730f
Mark-Soko <55922190+Mark-Soko@users.noreply.github.com>
2020-06-23 11:54:31 +0300

    Update connect.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,8 +1,8 @@
 <?php
-   $host='localhost';
-   $user ='root';
-   $pass ='';
-   $db_name ='test2';
+   $host='pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+   $user ='jqb2qn6bhob3ohyw';
+   $pass ='vj4ppfms4x9n3d6s';
+   $db_name ='gibge6sk3u3w8x4r';
 
    $conn = new MySQLi($host,$user,$pass,$db_name);
 
